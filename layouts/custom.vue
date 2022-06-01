9aa86d539ac713233eeee066627e5a241b7dc016
double <double@buzzwoo.de>
2022-06-02 01:59:08 +0700

    added tailwindcss and setup config nuxt js

---
+++ layouts/custom.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-between">
+    <div>
+      <Header />
+      <main class="container mx-auto my-5">
+        <slot />
+      </main>
+    </div>
+    <footer></footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-3xl">Index page</h1>
+    <div v-if="show">Show</div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  data() {
+    return {
+      show: true
+    }
+  }
+}
+</script>
